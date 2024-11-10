3e0e8d3e86a746ff441585998a62d556a83bd75d
xtimlin <xtimlin@gmail.com>
2024-11-09 23:22:45 -0500

    fix sidebar issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Sidebar from './components/sidebar/Sidebar';
+import * as FaIcons from 'react-icons/fa';
 import Home from './pages/Home';
 import ImageSearch from './pages/ImageSearch';
 import Todo from './pages/Todo';
+import './App.css'; // Assuming you put the CSS in this file
 
 const App: React.FunctionComponent = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <>
       <Router>
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/imageSearch" element={<ImageSearch />} />
-          <Route path="/todo" element={<Todo />} />
-        </Routes>
+        <div>
+          {/*TopNav*/}
+          <nav className="flex justify-between items-center bg-zinc-800 p-6 text-white fixed w-full top-0 left-0 z-50">
+            <button onClick={toggleSidebar} className="sidebar-toggle">
+              <FaIcons.FaBars />
+            </button>
+          </nav>
+
+          <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+          <div className={`main-content ${sidebarOpen ? 'shifted' : ''}`}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/imageSearch" element={<ImageSearch />} />
+              <Route path="/todo" element={<Todo />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </>
   );
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,43 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import { SidebarData } from './SidebarData';
 import './Sidebar.css';
 
-const Sidebar: React.FC = () => {
-  const [close, setClose] = useState(false);
-  const showSidebar = () => setClose(!close);
+interface SidebarProps {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}
 
+const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
   return (
-    <>
-      <nav className="flex items-center h-14 bg-blue-400">
-        <Link
-          to="#"
-          onClick={showSidebar}
-          className="flex justify-start ml-8 text-2xl text-white"
-        >
-          <FaIcons.FaBars />
-        </Link>
-      </nav>
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <button className="float-end flex mt-3 mr-4 text-2xl text-white ">
+        <FaIcons.FaTimes className="" onClick={toggleSidebar} />
+      </button>
 
-      <div className={`sidebar-menu ${close ? 'open' : 'closed'}`}>
-        <div className="flex justify-end mt-3 mr-4 text-2xl text-white">
-          <FaIcons.FaTimes onClick={showSidebar} />
-        </div>
-
-        {SidebarData.map((item, index) => (
-          <li key={index} className="flex items-center list-none px-0 py-4">
-            <Link
-              to={item.path}
-              className="flex items-center px-8 text-lg text-white no-underline menu-item-link"
-            >
-              {item.icon}
-              <span className="ml-4">{item.title}</span>
-            </Link>
-          </li>
-        ))}
-      </div>
-    </>
+      {SidebarData.map((item, index) => (
+        <li key={index} className="flex items-center list-none px-0 py-4">
+          <Link
+            to={item.path}
+            className="flex items-center px-8 text-lg text-white no-underline menu-item-link"
+          >
+            {item.icon}
+            <span className="ml-4">{item.title}</span>
+          </Link>
+        </li>
+      ))}
+    </div>
   );
 };
 
